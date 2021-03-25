5cbe00ed8d33b628f1f695e18da9266ab6df9499
garciajordy <jordygarcia0119@gmail.com>
2021-03-25 14:44:37 +0200

    Add user.rb file in lib folder and removed user class from main

---
+++ lib/user.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Class to make users
+class User
+  attr_reader :name
+
+  attr_writer :marker
+
+  def initialize(name)
+    name += "\'s" if name[-1] != 's'
+    @name = name
+  end
+
+  def mark
+    @marker
+  end
+  end
