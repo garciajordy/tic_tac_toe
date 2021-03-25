3a018ea3673b69e81462bd4087bb210180ff0d3a
Juliana Osemeke <juliana.osemeke@gmail.com>
2021-03-25 11:17:23 +0100

    Resolved rubocop conflict, change global variable to constant

--- lib/logic.rb
+++ lib/logic.rb
@@ -2,14 +2,20 @@
 
 # Game logic class
 class Game
-  @@possible_wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6],
-                     [1, 4, 7], [2, 5, 8], [0, 4, 8], [6, 4, 2]]
+  POSSIBLE_WINS = [[0, 1, 2],
+                   [3, 4, 5],
+                   [6, 7, 8],
+                   [0, 3, 6],
+                   [1, 4, 7],
+                   [2, 5, 8],
+                   [0, 4, 8],
+                   [6, 4, 2]].freeze
   def initialize
     @cells = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   def verify
-    @@possible_wins.any? do |arr|
+    POSSIBLE_WINS.any? do |arr|
       ((@cells[arr[0]] == @cells[arr[1]]) && (@cells[arr[0]] == @cells[arr[2]]))
     end
   end
